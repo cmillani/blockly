45d9c6e469babb33eb0ccfee7332cf0f8b347737
cmillani <carloseduardomillani@gmail.com>
2016-03-28 17:28:09 -0300

    Working version of the website, not uploading via BLE yet

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-var element = document.querySelector("#greeting");
-element.innerText = "Hello, world!";
+// var element = document.querySelector("#greeting");
+// element.innerText = "Hello, world!";
+// function jump(h) {
+// 	console.log(''')
+// 	var url = location.href;               //Save down the URL without hash.
+//   location.href = "#"+h;                 //Go to the target element.
+//   history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
+// }
 //0x20C38FF648C3
