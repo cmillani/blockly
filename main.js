558f2c9cc0c20a17fda7a279d6118574f369bddc
cmillani <carloseduardomillani@gmail.com>
2016-09-28 14:31:23 -0300

    Function to change current blocks

--- main.js
+++ main.js
@@ -7,3 +7,42 @@
 //   history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
 // }
 //0x20C38FF648C3
+
+var blocksXML = {
+	event_onstart: '<block type="event_onstart"></block>',
+	event_btdown: '<block type="event_btdown"></block>',
+	event_distancechange: '<block type="event_distancechange"><value name="threshold"><block type="math_number"><field name="NUM">20</field></block></value></block>',
+	controls_if: '<block type="controls_if"></block>',
+	logic_compare: '<block type="logic_compare"></block>',
+	math_number: '<block type="math_number"></block>',
+	distance: '<block type="distance"></block>',
+	led_red: '<block type="led_red"></block>',
+	led_green: '<block type="led_green"></block>',
+	go_forward: '<block type="go_forward"></block>',
+	go_left: '<block type="go_left"></block>',
+	go_right: '<block type="go_right"></block>',
+	stop: '<block type="stop"></block>',
+	step_forward: '<block type="step_forward"></block>',
+	turn_right: '<block type="turn_right"></block>',
+	turn_left: '<block type="turn_left"></block>',
+	controls_repeat_ext: '<block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block>',
+	controls_repeat_forever: '<block type= "controls_repeat_forever"></block>',
+	controls_whileUntil: '<block type="controls_whileUntil"></block>'
+};
+
+Object.freeze(blocksXML);
+
+function setGroupOfBlocks(workspace, blocks) {
+	var toolbox = "<xml>";
+	
+	for (var i in blocks) {
+		var newLine = blocksXML[blocks[i]];
+		if (newLine == undefined) {
+			throw new Error("Invalid Block");
+		}
+		toolbox += newLine;
+	}
+	
+	toolbox += "</xml>";
+	workspace.updateToolbox(toolbox);
+}
